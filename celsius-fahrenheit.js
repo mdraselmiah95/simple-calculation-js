384871c8b047369ca98e54870064b3dfceee8084
rasel <russelroy15@gmail.com>
2021-08-06 00:57:58 +0600

    some simple javascript calculation projects

---
+++ celsius-fahrenheit.js
@@ -0,0 +1,7 @@
+function celsiusToFahrenheit(celsius) {
+    var fahrenheit = celsius * 1.8 + 32;
+    return fahrenheit;
+}
+
+var toDayFahrenheit = celsiusToFahrenheit(33);
+console.log(toDayFahrenheit);
