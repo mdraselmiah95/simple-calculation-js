b21059f89a0916a54391a459638e8d633b7348c8
rasel <russelroy15@gmail.com>
2021-08-06 01:59:29 +0600

    interest rate calculation

---
+++ simple-Interest.js
@@ -0,0 +1,28 @@
+// Interest per months
+
+function interestPerMonth(principal, rateOfInterest, time) {
+    var simpleInterest = (principal * rateOfInterest * time) / 100;
+    return simpleInterest;
+}
+
+
+
+
+var myMonthlyInterest = interestPerMonth(100000, 8, 1);
+console.log(myMonthlyInterest);
+
+
+
+
+
+
+
+
+
+
+
+// SI= P x R x T ÷ 100,
+// where P = Principal,
+// R = Rate of Interest,
+// T = Time Period of the Loan/deposit in years,
+// SI= Simple Interest
